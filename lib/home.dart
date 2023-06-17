9b14b1ba427cf890cf6fd176e188450b64af68bf
Nerses <nerses@zakoyan.com>
2023-06-17 17:44:12 +0300

    Add existing project files to Git

---
+++ lib/home.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'dart:math' as math;
+
+import 'map_zoom_buttons.dart';
+
+const List<String> list = <String>[
+  'Все',
+  'ЖКХ',
+  'Здоровье',
+  'Демография',
+  'Транспорт'
+];
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Map<LatLng, Color> pointsData = {};
+  bool isSatellite = false;
+
+  String dropdownValue = list.first;
+  double _currentSliderValue = 2023;
+
+  @override
+  void initState() {
+    generatePointsData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+            child: FlutterMap(
+          options: MapOptions(
+            interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+            center: LatLng(45.036349, 38.974551),
+            zoom: 6.5,
+          ),
+          nonRotatedChildren: [
+            Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isSatellite = !isSatellite;
+                  });
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(4)),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  child: TileLayer(
+                    urlTemplate:
+                        "https://api.mapbox.com/styles/v1/mapbox/{styleId}/tiles/512/{z}/{x}/{y}@2x?access_token={accessToken}",
+                    additionalOptions: {
+                      "accessToken":
+                          "pk.eyJ1Ijoibnpha295YSIsImEiOiJjbGl6dHRzaWQwcTFvM2lxcXJ5dWJ4a212In0.VxYqYQWT0bAVKk2TVGtJAQ",
+                      "styleId": isSatellite ? "streets-v12" : "satellite-v9"
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.arrow_downward),
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? value) {
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
+                    items: list.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                )),
+            const Positioned(
+                right: 12,
+                top: 70,
+                child: FlutterMapZoomButtons(
+                  maxZoom: 12.5,
+                  zoomInColor: Colors.white,
+                  zoomOutColor: Colors.white,
+                  zoomOutIcon: Icons.remove,
+                  zoomInIcon: Icons.add,
+                  padding: 0,
+                  mini: true,
+                  alignment: Alignment.topRight,
+                )),
+          ],
+          children: [
+            TileLayer(
+              urlTemplate:
+                  "https://api.mapbox.com/styles/v1/mapbox/{styleId}/tiles/512/{z}/{x}/{y}@2x?access_token={accessToken}",
+              additionalOptions: {
+                "accessToken":
+                    "pk.eyJ1Ijoibnpha295YSIsImEiOiJjbGl6dHRzaWQwcTFvM2lxcXJ5dWJ4a212In0.VxYqYQWT0bAVKk2TVGtJAQ",
+                "styleId": isSatellite ? "satellite-v9" : "streets-v12"
+              },
+            ),
+            MarkerLayer(
+                markers: pointsData.entries
+                    .map((entry) => Marker(
+                          point: getRandomLocation(entry.key, 5000),
+                          width: 30,
+                          height: 30,
+                          builder: (context) => Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: entry.value),
+                          ),
+                        ))
+                    .toList())
+          ],
+        )),
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(children: [
+              const Text('2023'),
+              Expanded(
+                  child: Slider(
+                divisions: 27,
+                value: _currentSliderValue,
+                min: 2023,
+                max: 2050,
+                label: _currentSliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                  });
+                },
+              )),
+              const Text('2050')
+            ]))
+      ],
+    ));
+  }
+
+  LatLng getRandomLocation(LatLng point, int radius) {
+    //This is to generate 10 random points
+    double x0 = point.latitude;
+    double y0 = point.longitude;
+
+    math.Random random = math.Random();
+
+    // Convert radius from meters to degrees
+    double radiusInDegrees = radius / 111000;
+
+    double u = random.nextDouble();
+    double v = random.nextDouble();
+    double w = radiusInDegrees * math.sqrt(u);
+    double t = 2 * pi * v;
+    double x = w * math.cos(t);
+    double y = w * math.sin(t) * 1.75;
+
+    // Adjust the x-coordinate for the shrinking of the east-west distances
+    double new_x = x / math.sin(y0);
+
+    double foundLatitude = new_x + x0;
+    double foundLongitude = y + y0;
+    LatLng randomLatLng = new LatLng(foundLatitude, foundLongitude);
+
+    return randomLatLng;
+  }
+
+  generatePointsData() {
+    pointsData = {
+      for (var index in List.generate(100, (index) => index))
+        getRandomLocation(LatLng(45.036349, 38.974551), 5000): Color.fromRGBO(
+            math.Random().nextInt(255), math.Random().nextInt(255), 0, 0.5)
+    };
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:hack/home_3d.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'iFuture'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  PageController? _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: _currentIndex);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    backgroundColor: Colors.transparent,
+                    builder: (context) {
+                      return SafeArea(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                              top: 100,
+                            ),
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(8)),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Expanded(child: SizedBox()),
+                                    Expanded(
+                                        child: Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 16),
+                                            child: SizedBox(
+                                                width: 36,
+                                                height: 4,
+                                                child: FittedBox(
+                                                    child: Container(
+                                                  width: 36,
+                                                  height: 4,
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                          color: Colors.black87,
+                                                          borderRadius:
+                                                              BorderRadius.all(
+                                                                  Radius
+                                                                      .circular(
+                                                                          4))),
+                                                ))))),
+                                    Expanded(
+                                        child: IconButton(
+                                            alignment:
+                                                AlignmentDirectional.centerEnd,
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            icon: const Icon(Icons.close)))
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 200,
+                                  height: 75,
+                                  child: TextField(
+                                    decoration:
+                                        InputDecoration(hintText: "Логин"),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 200,
+                                  height: 75,
+                                  child: TextField(
+                                    obscureText: true,
+                                    decoration:
+                                        InputDecoration(hintText: "Пароль"),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                      foregroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.deepPurpleAccent)),
+                                  onPressed: () {},
+                                  child: const Text('Войти'),
+                                ),
+                              ],
+                            )),
+                      );
+                    });
+              },
+              icon: const Icon(Icons.person))
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [Home(), Home3D()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) async {
+            await _pageController?.animateToPage(index,
+                duration: const Duration(milliseconds: 100),
+                curve: Curves.easeIn);
+            setState(() => _currentIndex = index);
+          },
+          items: const [
+            BottomNavigationBarItem(
+                label: 'Прогроз', icon: Icon(Icons.access_time)),
+            BottomNavigationBarItem(
+                label: '3D Прогноз', icon: Icon(Icons.home_work_outlined)),
+          ]),
+    );
+  }
+}
